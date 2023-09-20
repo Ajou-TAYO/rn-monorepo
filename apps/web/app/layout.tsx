3acfe2ce4a67e57ad9e2605ddfa179198e2509b9
User <user@Userui-MacBookAir.local>
2023-09-20 13:53:30 +0900

    commit

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,7 +9,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body>
-                <Resizer>{children}</Resizer>
+                {/* <Resizer> */}
+                {children}
+                {/* </Resizer> */}
             </body>
         </html>
     );
